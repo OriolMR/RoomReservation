ae4a09bed10d91ee5e64b58643404dffbb916cb4
GuillermoJM-Inetum <guillermo.jaume@inetum.com>
2023-07-17 13:42:08 +0200

    VACIO

--- RoomReservation/webapi/Models/UpdateUserModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace webapi.Models
-{
-    public class UpdateUserModel
-    {
-        public string newUserName { get; set; }
-        public string newEmail { get; set; }
-        public string newPasswordHash { get; set; }
-    }
-}
